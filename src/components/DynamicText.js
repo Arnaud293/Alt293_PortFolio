3ab16894f1071509ef8b5bd8fe77e330c68537c9
Arnaud293 <arnaudlt293@gmail.com>
2022-12-02 15:44:01 +0100

    preparing home page, add footer,nav,dynamic text

---
+++ src/components/DynamicText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicText = () => {
+    return (
+        <div className='dynamic-text'>
+            <span>React Developer</span>
+        </div>
+    );
+};
+
+export default DynamicText;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import DynamicText from '../components/DynamicText';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
-        <div>
-            <Nav />
+        <>
+        <Nav />
+        <div className='home'>
+            <DynamicText />
+            <div className="home-cards-container">
+                <div className="home-cards-top">
+                    <p>Who am I ? 😎</p>
+                </div>
+                <div className="home-cards-top">
+                    <p>Get in touch ✔️</p>
+                </div>
+                <div className="home-cards-bottom">
+                    <p>View my work 👀</p>
+                </div>
+            </div>
+            
         </div>
+        <Footer />
+        </>
     );
 };
 
