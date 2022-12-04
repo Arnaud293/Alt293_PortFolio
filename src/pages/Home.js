1b6bfae75e821dd05fb3d54f154d9152a3bac5f5
Arnaud293 <arnaudlt293@gmail.com>
2022-12-04 15:05:44 +0100

    add navlinks on cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import Footer from '../components/Footer';
 import ProfilPic from '../assets/img/me.webp';
 import CallPic from '../assets/img/call.jpg';
 import KanapPreview from '../assets/img/Kanap.png';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 
@@ -20,39 +21,45 @@ const Home = () => {
         <div className='home'>
             <DynamicText />
             <div className="home-cards-container">
-                <div className="home-cards-top" onMouseEnter={() => setWhoIsShown(true)} onMouseLeave={() => setWhoIsShown(false)}>
-                    {whoIsShown ?  (
-                        <div className='home-cards-top-about-reverse'>
-                            <p>LEARN MORE ABOUT ME</p>
-                            <img src={ProfilPic} alt='profil-picture-arnaud' className='home-cards-top-profil-picture' />
-                        </div>
-                    ) : (
-                        <p>Who am I ? 😎</p>
-                    )}
-                    
-                </div>
-                <div className="home-cards-top" onMouseEnter={() => setInTouchIsShown(true)} onMouseLeave={() => setInTouchIsShown(false)}>
-                    {inTouchIsShown ? (
-                        <div className='home-cards-top-contact-reverse'>
-                            <p>LET'S GET IN TOUCH 🤳</p>
-                            <img src={CallPic} alt='woman-call-picture' className='home-cards-top-contact-picture' />
-                        </div>
-                    ) : (
-                        <p>Get in touch ✔️</p>
-                    )}
-                    
-                </div>
-                <div className="home-cards-bottom" onMouseEnter={() => setWorkIsShown(true)} onMouseLeave={() => setWorkIsShown(false)}>
-                    {workIsShown ? (
-                        <div className='home-cards-top-work-reverse'>
-                            <p>TAKE A LOOK 👀</p>
-                            <img src={KanapPreview} alt='project-preview' className='home-cards-top-work-picture' />
-                        </div>
-                    ):(
-                        <p>View my work 👀</p>
-                    )}
-                    
-                </div>
+                <NavLink to='/about'>
+                    <div className="home-cards-top" onMouseEnter={() => setWhoIsShown(true)} onMouseLeave={() => setWhoIsShown(false)}>
+                        {whoIsShown ?  (
+                            <div className='home-cards-top-about-reverse'>
+                                <p>LEARN MORE ABOUT ME</p>
+                                <img src={ProfilPic} alt='profil-picture-arnaud' className='home-cards-top-profil-picture' />
+                            </div>
+                        ) : (
+                            <p>Who am I ? 😎</p>
+                        )}
+                        
+                    </div>
+                </NavLink>
+                <NavLink to='/contact'>
+                    <div className="home-cards-top" onMouseEnter={() => setInTouchIsShown(true)} onMouseLeave={() => setInTouchIsShown(false)}>
+                        {inTouchIsShown ? (
+                            <div className='home-cards-top-contact-reverse'>
+                                <p>LET'S GET IN TOUCH 🤳</p>
+                                <img src={CallPic} alt='woman-call-picture' className='home-cards-top-contact-picture' />
+                            </div>
+                        ) : (
+                            <p>Get in touch ✔️</p>
+                        )}
+                        
+                    </div>
+                </NavLink>
+                <NavLink to='/projects'>
+                    <div className="home-cards-bottom" onMouseEnter={() => setWorkIsShown(true)} onMouseLeave={() => setWorkIsShown(false)}>
+                        {workIsShown ? (
+                            <div className='home-cards-top-work-reverse'>
+                                <p>TAKE A LOOK 👀</p>
+                                <img src={KanapPreview} alt='project-preview' className='home-cards-top-work-picture' />
+                            </div>
+                        ):(
+                            <p>View my work 👀</p>
+                        )}
+                        
+                    </div>
+                </NavLink>
             </div>
             <Footer />
         </div>
