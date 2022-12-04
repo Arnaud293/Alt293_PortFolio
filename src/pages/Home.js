c0a8d40e72504d4cdc5ba179b8c2517cb7431ccd
Arnaud293 <arnaudlt293@gmail.com>
2022-12-04 13:12:00 +0100

    preparing dynamic cards on hover with states

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../components/Nav';
 import DynamicText from '../components/DynamicText';
 import Footer from '../components/Footer';
 
 const Home = () => {
+
+    const [whoIsShown, setWhoIsShown] = useState(false);
+    const [inTouchIsShown, setInTouchIsShown] = useState(false);
+    const [workIsShown, setWorkIsShown] = useState(false);
+
     return (
         <>
         <Nav />
         <div className='home'>
             <DynamicText />
             <div className="home-cards-container">
-                <div className="home-cards-top">
-                    <p>Who am I ? 😎</p>
+                <div className="home-cards-top" onMouseEnter={() => setWhoIsShown(true)} onMouseLeave={() => setWhoIsShown(false)}>
+                    {whoIsShown ?  (
+                        <p>Hover style here</p>
+                    ) : (
+                        <p>Who am I ? 😎</p>
+                    )}
+                    
                 </div>
-                <div className="home-cards-top">
-                    <p>Get in touch ✔️</p>
+                <div className="home-cards-top" onMouseEnter={() => setInTouchIsShown(true)} onMouseLeave={() => setInTouchIsShown(false)}>
+                    {inTouchIsShown ? (
+                        <p>Hover style here</p>
+                    ) : (
+                        <p>Get in touch ✔️</p>
+                    )}
+                    
                 </div>
-                <div className="home-cards-bottom">
-                    <p>View my work 👀</p>
+                <div className="home-cards-bottom" onMouseEnter={() => setWorkIsShown(true)} onMouseLeave={() => setWorkIsShown(false)}>
+                    {workIsShown ? (
+                        <p>Hover style here</p>
+                    ):(
+                        <p>View my work 👀</p>
+                    )}
+                    
                 </div>
             </div>
             <Footer />
