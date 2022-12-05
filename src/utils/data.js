80c46862b58785f7ed5505db74d8bd44f2dd7b99
Arnaud293 <arnaudlt293@gmail.com>
2022-12-05 12:13:56 +0100

    create data object for projects

---
+++ src/utils/data.js
@@ -0,0 +1,43 @@
+
+export const data = [
+    {
+        id: 1,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 2,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 3,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 4,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 5,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 6,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 7,
+        title : "",
+        img: '../assets/img/groupo.png',
+    },
+    {
+        id: 8,
+        title : "",
+        img: '../assets/img/groupo.png',
+    }
+]
