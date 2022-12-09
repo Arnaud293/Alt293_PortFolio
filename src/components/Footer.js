82546a25103d84a5667f0aabbe3fd218b8c97863
Arnaud293 <arnaudlt293@gmail.com>
2022-12-09 18:41:35 +0100

    add link to icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linkedin"></i>
+            <a href='https://github.com/Arnaud293' target='_blank'>
+                <i class="fa-brands fa-github"></i>
+            </a>
+            <a href='https://www.linkedin.com/in/arnaud-le-tallec-106b571a7/' target='_blank'>
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
         </div>
     );
 };
