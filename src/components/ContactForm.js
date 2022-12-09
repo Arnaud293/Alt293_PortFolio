05b16167e7c4effeec07c47e7c0f33927b71e943
Arnaud293 <arnaudlt293@gmail.com>
2022-12-09 17:34:59 +0100

    Contact is responsive + toggle for resume

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,11 +6,14 @@ import ResumePreview from '../assets/img/resume.png'
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import ResumePdf from '../assets/CV_Arnaud_LeTallec_JSDeveloper.pdf';
+import { useEffect } from 'react';
 
 export const ContactUs = () => {
     const form = useRef();
     
     const [isShown, setIsShown] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    const [resumeResponsive, setResumeResponsive] = useState(false);
 
     const sendEmail = (e) => {
       e.preventDefault();
@@ -33,24 +36,50 @@ export const ContactUs = () => {
             }, 2500)
         });
     };
+    useEffect(() => {
+
+        const handleScreen = () => {
+            setWidth(window.innerWidth);
+            if(window.innerWidth <= 500){
+                setResumeResponsive(true);
+            }
+            else{
+                setResumeResponsive(false);
+            }
+            
+        }
+        window.addEventListener('resize', handleScreen);
+        return () => window.removeEventListener('resize', handleScreen);
+
+    },[])
+    
   
     return (
       <div className="form-container">
             <div className="form-container-left" >
-                <div className='form-container-left-text' onClick={()=>{navigator.clipboard.writeText("06 06 45 72 15"); alert('Numéro copié dans le presse-papier')}}>
-                    <p>06 06 45 72 15</p>
-                </div>
-                <div className='form-container-left-text' onClick={()=>{navigator.clipboard.writeText("arnaudlt293@gmail.com"); alert('Email copié dans le presse-papier')}}>
-                    <p>arnaudlt293@gmail.com</p>
+                <div className="form-container-left-content">
+                    <div className='form-container-left-content-text' onClick={()=>{navigator.clipboard.writeText("06 06 45 72 15"); alert('Numéro copié dans le presse-papier')}}>
+                        <p>06 06 45 72 15</p>
+                    </div>
+                    <div className='form-container-left-content-text' onClick={()=>{navigator.clipboard.writeText("arnaudlt293@gmail.com"); alert('Email copié dans le presse-papier')}}>
+                        <p>arnaudlt293@gmail.com</p>
+                    </div>
                 </div>
                 <NavLink to={ResumePdf} target='_blank' rel='noopener noreferrer'>
+                    {resumeResponsive === false ? (
                     <div className="form-container-left-resume" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
                         <img src={ResumePreview} alt="resume-arnaud" />
                         {isShown && (
                             <i class="fa-solid fa-eye"></i>
                         )}
                     </div>
+                    ):(
+                        <div className='form-container-left-content-text'>
+                            <p>View resume ... <i className="fa-solid fa-download"></i></p>
+                        </div>
+                    )}
                 </NavLink>
+
             </div>
             <div className="form-container-right">
                 <form ref={form} onSubmit={sendEmail}>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactForm from '../components/ContactForm';
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 const Contact = () => {
     return (
@@ -8,6 +9,7 @@ const Contact = () => {
             <Nav />
             <h1 className='contact-title'>Just get in touch ! 👋</h1>
             <ContactForm />
+            <Footer />
         </div>
     );
 };
